8313317b98d0356fef51af8546be02cc6e3a1c95
dsumicroproject123 <119723084+dsumicroproject123@users.noreply.github.com>
2023-09-23 20:59:47 +0530

    Update webauthn.js

--- webauthn.js
+++ webauthn.js
@@ -1,63 +1,103 @@
-const registerButton = document.getElementById("registerButton");
-const loginButton = document.getElementById("loginButton");
-const registrationResult = document.getElementById("registrationResult");
-const loginResult = document.getElementById("loginResult");
+// Check for WebAuthn support
+if (!window.PublicKeyCredential) {
+    alert('WebAuthn is not supported in this browser.');
+} else {
+    // Select the registration and login buttons
+    const registerButton = document.getElementById('registerButton');
+    const loginButton = document.getElementById('loginButton');
 
-registerButton.addEventListener("click", async () => {
-    try {
-        // Request a credential creation
-        const publicKeyCredential = await navigator.credentials.create({
-            publicKey: {
-                // User and challenge information
-                rp: { name: "github.com"},
-                user: { id: new Uint8Array(16), name: "harshalkanaskar1998@gmail.com", displayName: "Harshal Kanaskar" },
-                challenge: new Uint8Array(32),
+    // Register button click event
+    registerButton.addEventListener('click', () => {
+        const username = 'YourUsername';  // Replace with the actual username
+        const user_id = generateRandomUserID();  // Generate a random user ID
+        fetch('/register', { method: 'POST', body: JSON.stringify({ username, user_id }) })
+            .then(response => response.json())
+            .then(options => {
+                return navigator.credentials.create({ publicKey: options });
+            })
+            .then((credential) => {
+                // Send the credential to the server for storage
+                return fetch('/register/verify', { method: 'POST', body: JSON.stringify({ username, user_id, ...credential.response }) });
+            })
+            .then(response => response.json())
+            .then(result => {
+                // Handle successful registration
+                displayRegistrationResult(result.message);
+            })
+            .catch((error) => {
+                // Handle registration error
+                displayRegistrationResult('Registration failed: ' + error.message);
+            });
+    });
 
-                // Specify the desired authenticator
-                authenticatorSelection: {
-                    userVerification: "required",
-                    requireResidentKey: false,
-                },
+    // Login button click event
+    loginButton.addEventListener('click', () => {
+        const user_id = "MyGitHubWebAuthDemo";  // Replace with the actual user ID
+        fetch('/login', { method: 'POST', body: JSON.stringify({ user_id }) })
+            .then(response => response.json())
+            .then(options => {
+                return navigator.credentials.get({ publicKey: options });
+            })
+            .then((credential) => {
+                // Send the credential to the server for verification
+                return fetch('/login/verify', { method: 'POST', body: JSON.stringify({ user_id, ...credential.response }) });
+            })
+            .then(response => response.json())
+            .then(result => {
+                // Handle successful login
+                displayLoginResult(result.message);
+            })
+            .catch((error) => {
+                // Handle login error
+                displayLoginResult('Login failed: ' + error.message);
+            });
+    });
 
-                // Specify the allowed credential types (e.g., public-key or password)
-                pubKeyCredParams: [
-                    { type: "public-key", alg: -7 },
-                ],
-            },
-        });
+    // Generate PublicKeyCredential creation or request options
+    function generatePublicKeyOptions(isLogin = false) {
+        const challenge = generateRandomBuffer(32);
+        const username = 'MyGitHubWebAuthDemo';  // Replace with the actual username
+        const user_id = isLogin ? 'YourUserID' : generateRandomUserID();  // Generate a random user ID for registration
+        const rp = { id: 'https://dsumicroproject123.github.io', name: 'Harshal kanaskar' };
+        const pubKeyCredParams = [{ type: 'public-key', alg: -7 }];
+        const timeout = 60000;
+        const excludeCredentials = isLogin ? [] : null;
+        const authenticatorSelection = { authenticatorAttachment: 'platform', userVerification: 'required' };
+        const attestation = isLogin ? 'none' : 'direct';
 
-        // Send the credential to the server for validation and storage
-        // Store publicKeyCredential on the server
-
-        // Display registration result
-        registrationResult.textContent = "Credential registered successfully.";
-    } catch (error) {
-        // Display registration error
-        registrationResult.textContent = "Registration failed: " + error;
+        return {
+            challenge,
+            user: { id: new TextEncoder().encode(user_id), name: username, displayName: username },
+            rp,
+            pubKeyCredParams,
+            timeout,
+            excludeCredentials,
+            authenticatorSelection,
+            attestation,
+        };
     }
-});
 
-loginButton.addEventListener("click", async () => {
-    try {
-        // Request an authentication
-        const publicKeyCredential = await navigator.credentials.get({
-            publicKey: {
-                challenge: new Uint8Array(32),
-                rpId: "WebAuthn Demo",
-
-                allowCredentials: [
-                    // Retrieve the user's registered credentials from the server
-                    // (They must have been stored during registration)
-                ],
-            },
-        });
-
-        // Send the publicKeyCredential to the server for validation
-
-        // Display authentication result
-        loginResult.textContent = "Authentication successful.";
-    } catch (error) {
-        // Display authentication error
-        loginResult.textContent = "Authentication failed: " + error;
+    // Display registration result
+    function displayRegistrationResult(result) {
+        const registrationResult = document.getElementById('registrationResult');
+        registrationResult.textContent = result;
     }
-});
+
+    // Display login result
+    function displayLoginResult(result) {
+        const loginResult = document.getElementById('loginResult');
+        loginResult.textContent = result;
+    }
+
+    // Generate a random ArrayBuffer
+    function generateRandomBuffer(length) {
+        const array = new Uint8Array(length);
+        window.crypto.getRandomValues(array);
+        return array.buffer;
+    }
+
+    // Generate a random user ID (for registration)
+    function generateRandomUserID() {
+        return 'MyGitHubWebAuthDemo';
+    }
+}
