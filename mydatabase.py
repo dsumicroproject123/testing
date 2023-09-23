02dde74c4fc4d16b0b9715fdb4d41a2fc58347ff
dsumicroproject123 <119723084+dsumicroproject123@users.noreply.github.com>
2023-09-23 20:15:33 +0530

    Update mydatabase.py

--- mydatabase.py
+++ mydatabase.py
@@ -1,29 +1,14 @@
 import sqlite3
+conn = sqlite3.connect('mydatabase.db')
+cursor = conn.cursor()
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY,
+        username TEXT NOT NULL,
+        email TEXT NOT NULL,
+        credential_key TEXT
+    )
+''')
 
-# Function to establish a database connection
-def get_db_connection():
-    conn = sqlite3.connect('mydatabase.db')
-    return conn
-
-def create_user(username, email, credential_key):
-    try:
-        conn = get_db_connection()
-        cursor = conn.cursor()
-        cursor.execute("INSERT INTO users (username, email, credential_key) VALUES (?, ?, ?)",
-                       (username, email, credential_key))
-        conn.commit()
-        return True
-    except Exception as e:
-        print(f"Error creating user: {e}")
-        return False
-
-def get_user_by_username(username):
-    try:
-        conn = get_db_connection()
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM users WHERE username=?", (username,))
-        user = cursor.fetchone()
-        return user
-    except Exception as e:
-        print(f"Error fetching user: {e}")
-        return None
+conn.commit()
+conn.close()
