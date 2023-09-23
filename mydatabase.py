a898ad362708416ab77958b99190763913b26209
dsumicroproject123 <119723084+dsumicroproject123@users.noreply.github.com>
2023-09-23 20:52:50 +0530

    Update mydatabase.py

--- mydatabase.py
+++ mydatabase.py
@@ -1,14 +1,44 @@
 import sqlite3
-conn = sqlite3.connect('mydatabase.db')
-cursor = conn.cursor()
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY,
-        username TEXT NOT NULL,
-        email TEXT NOT NULL,
-        credential_key TEXT
-    )
-''')
 
-conn.commit()
-conn.close()
+DATABASE_NAME = 'webauthn_demo.db'
+
+def create_tables():
+    conn = sqlite3.connect(DATABASE_NAME)
+    cursor = conn.cursor()
+
+    # Create a table for registered users
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY,
+            username TEXT NOT NULL,
+            user_id TEXT NOT NULL UNIQUE
+        )
+    ''')
+
+    conn.commit()
+    conn.close()
+
+def add_user(username, user_id):
+    conn = sqlite3.connect(DATABASE_NAME)
+    cursor = conn.cursor()
+
+    try:
+        cursor.execute("INSERT INTO users (username, user_id) VALUES (?, ?)", (username, user_id))
+        conn.commit()
+        return True
+    except sqlite3.IntegrityError:
+        return False
+    finally:
+        conn.close()
+
+def is_user_registered(user_id):
+    conn = sqlite3.connect(DATABASE_NAME)
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT 1 FROM users WHERE user_id = ?", (user_id,))
+    result = cursor.fetchone()
+
+    conn.close()
+
+    return result is not None
+
